ac43b3181aea4c238f8088d47e74d9be2e9c9eeb
LauraLemoine <lolo83340@hotmail.fr>
2019-03-27 15:49:39 +0100

    Add function multiplier

--- test/test.js
+++ test/test.js
@@ -25,6 +25,10 @@ function soustraire(nombre1, nombre2) {
    return nombre1 - nombre2; 
 }
 
+function multiplier(nombre1, nombre2) {
+   return nombre1 * nombre2;
+}
+
 //console log avec appel function avec definition des params 
 console.log(addition(n1,50)); 
 console.log('');
